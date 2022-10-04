e1bc3a69267f1754c655a53a71fecb04b62948de
Justina <justinakliu@gmail.com>
2022-10-04 13:25:31 -0400

    updates

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+from school_schedule.student import Student
 
-
-#first instance
+# first instance
 quinn = Student(
-                "Quinn", 
-                "junior", 
-                [
-                    "Pre-Calc", 
-                    "English III", 
-                    "World History", 
-                    "Gym", 
-                    "Chemistry", 
-                    "Music Composition"
-                ]
-            )
+    "Quinn",
+    "junior",
+    [
+        "Pre-Calc",
+        "English III",
+        "World History",
+        "Gym",
+        "Chemistry",
+        "Music Composition"
+    ]
+)
 
 quinn.add_class("Painting")
 quinn.get_num_classes()
@@ -20,17 +20,17 @@ quinn.summary()
 
 # second instance
 claire = Student(
-                "Claire", 
-                "freshmen", 
-                [
-                    "Algebra", 
-                    "Writing", 
-                    "Contemporary Issues", 
-                    "Gym", 
-                    "Earth Science", 
-                    "Painting"
-                ]
-            )
+    "Claire",
+    "freshmen",
+    [
+        "Algebra",
+        "Writing",
+        "Contemporary Issues",
+        "Gym",
+        "Earth Science",
+        "Painting"
+    ]
+)
 
 claire.get_num_classes()
 claire.summary()
